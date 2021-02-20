2d9b01db2a3cdb84571b751e39d827211da39683
Arthur Viana <eantropixdev@outlook.com>
2021-02-20 11:09:31 -0300

    Added Tarefa2

---
+++ Tarefa2/tarefa2.py
@@ -0,0 +1,13 @@
+# Tarefa 2: Utilizar o for loop para mostrar os números pares de 0 a 1000
+
+# Método convencional
+
+# for x in range(1001):
+#     if (x % 2 == 0):
+#         print(x, end=', ')
+
+# One-liner
+# print([x for x in range(101) if x % 2 == 0])
+
+
+print('stuff')
