78179a89ba0868b844a491b5e4971b4c04f19bf3
Arthur Viana <eantropixdev@outlook.com>
2021-04-08 19:15:44 -0300

    Finished project

--- Trabalho2/server.js
+++ Trabalho2/server.js
@@ -1,22 +1,12 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+const express = require('express');
+const app = express();
+const port = 8000;
+const path = require('path');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello World!\n')
-});
-
-server.listen(port, hostname, () => {
-    console.log('Server running at ' + 'http://' + hostname + ':' + port + '/');
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-// const express = require('express')
-// const app = express()
+app.use(express.static('./'));
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
-
-// app.listen(3000, () => console.log('Server ready'))
+app.listen(port, () => { console.log('Process PID ' + process.pid) });
